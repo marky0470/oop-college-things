319b9439a08c50c89a1afb80505d6b65c044eb8c
marky0470 <mark.soriano04@yahoo.com>
2022-06-06 10:06:08 +0800

    the big sad

--- ddo.py
+++ ddo.py
@@ -1,92 +1,77 @@
 import random
 
 class Puzzle:
-    """
-    The Puzzle object is what handles the main game. It is in charge of initializing and storing the Button objects, taking input from the user, flipping the neighboring buttons' 
-    states and checking for completion
-
-    attributes:
-        gameBoard: stores Button objects
-        gameDifficulty: stores the difficulty specified by the user
-    """
-
+    
     def __init__(self):
         self.buttonList = [
-        Button(1, (2,4)),
-        Button(2, (1,5,3)),
-        Button(3, (2,6)),
-        Button(4, (1,5,7)),
-        Button(5, (2,4,6,8)),
-        Button(6, (3,5,9)),
-        Button(7, (4,8)),
-        Button(8, (7,5,9)),
-        Button(9, (6,8))
-        ]                                                               
+        Button(1, (2,4),     [5]),
+        Button(2, (1,5,3),   (4,6)),
+        Button(3, (2,6),     [5]),
+        Button(4, (1,5,7),   (2,8)),
+        Button(5, (2,4,6,8), (1,3,7,9)),
+        Button(6, (3,5,9),   (2,8)),
+        Button(7, (4,8),     [5]),
+        Button(8, (7,5,9),   (4,6)),
+        Button(9, (6,8),     [5])
+        ]
+
         self.gameDifficulty = int(input("Available difficulties\nEasy\t[1]\nMedium\t[2]\nHard\t[3]\nExpert\t[4]\nPlease select a difficulty: "))
+        if self.gameDifficulty == 1:
+            self.isCardinal = True
+        else:
+            self.isCardinal = False
         self.puzzleGameplay()
 
     def displayBoard(self):
-        """
-        Displays the buttons in a 3x3 grid, note that True is printed as an "X" and False is printed as an "O"
-        """
-        for boardRow in range(3):
-            for boardColumn in range(3):
-                print("X" if self.buttonList[boardRow + boardColumn].isEnabled else "O", end=" ")
-            print("")
+
+        currentColumn = 0
+        for eachButton in self.buttonList:
+            print("X" if eachButton.isEnabled else "O", end = " ")
+            currentColumn += 1
+            if currentColumn == 3:
+                print("")
+                currentColumn = 0
 
     def isIncomplete(self):
-        """
-        Checks if the puzzle is incomplete
-        Return True if incomplete then return value will be used inside the for loop
-        """
+
         for currentButton in self.buttonList:
-            if currentButton.isEnabled:
-                return False
-            return True 
+            if currentButton.isEnabled == False:
+                return True
+        return False
 
     def puzzleGameplay(self):
-        """
-        Main game loop;
-            1. ask the user for the number of the button they wish to flip
-            2. flip said button and all its neighbors
-            3. check if all buttons are the same, if not, return to step 1
-        """
+
         while self.isIncomplete():
             self.displayBoard()
 
-            # You can access all of the board's contents here
-            #_gameBoard = self.gameBoard.buttonList
-            # This is where we get which button the user would like to select
             inputButton = int(input("Enter button number: "))
+
+            for eachButton in self.buttonList:
+                if eachButton.buttonNumber == inputButton:
+                    eachButton.changeState()
+                    if self.isCardinal == True:
+                        inputNeighbors = eachButton.cardinalNeighbors
+                    else:
+                        inputNeighbors = eachButton.diagonalNeighbors
             
-            '''
-            Task 1
-            Requirement:
-            Buttons adjacent to the inputButton should have their values flipped e.g. True -> False; False -> True
-            '''
+            for eachButton in self.buttonList:
+                if eachButton.buttonNumber in inputNeighbors:
+                    eachButton.changeState()
 
-            '''
-            Task 2
-            Requirement:
-            Buttons to the diagonals of the inputButton should have their values flipped
-            '''
-
-            '''
-            Task 3
-            Requirement:
-            Should make the flipping neighbors alternate between adjacent and diagonal 
-            '''
+            if self.gameDifficulty == 3:
+                self.isCardinal = not(self.isCardinal)
+            elif self.gameDifficulty == 4:
+                self.isCardinal = random.choice([True, False])
+        
+        print("Puzzle Complete")
 
 class Button:
-    """
-    The Button object stores its number/index, its state (True/False) and its neighbors. Through the changeState method it can have 
-    its isEnabled value flipped
-    """
 
-    def __init__(self, number, neighbors1):
+    def __init__(self, number, neighbors1, neighbors2):
         self.isEnabled = random.choice([True,False])
         self.buttonNumber = number
         self.cardinalNeighbors = neighbors1
+        self.diagonalNeighbors = neighbors2
     
     def changeState(self):
         self.isEnabled ^= 1
